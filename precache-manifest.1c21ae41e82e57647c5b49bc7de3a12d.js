d6fc4f1ceb053ae1abca3dbc5238d6e7a6cb5395
Kristina <k.poderyte@gmail.com>
2020-08-25 23:56:20 +0300

    Updates

---
+++ precache-manifest.1c21ae41e82e57647c5b49bc7de3a12d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ecffcee5d4aaf0be6dce03d26cb668",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "ce350d0b02658bd52d49",
+    "url": "/movies/static/css/2.9109016a.chunk.css"
+  },
+  {
+    "revision": "4af9bb5a54c7a2b5bb7c",
+    "url": "/movies/static/css/main.56371d78.chunk.css"
+  },
+  {
+    "revision": "ce350d0b02658bd52d49",
+    "url": "/movies/static/js/2.c9101a5a.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/movies/static/js/2.c9101a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af9bb5a54c7a2b5bb7c",
+    "url": "/movies/static/js/main.7a499982.chunk.js"
+  },
+  {
+    "revision": "b406ebb1a15d1e317a80",
+    "url": "/movies/static/js/runtime-main.b1cdf11b.js"
+  },
+  {
+    "revision": "c104ff306df4c0f93fe8719ef8156526",
+    "url": "/movies/static/media/movie.c104ff30.png"
+  }
+]);
