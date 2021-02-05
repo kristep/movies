d035c745ee364322114703ba5d33020d7f68e410
Kristina Stepanaviciene <kr.stepanaviciene@gmail.com>
2021-02-05 20:29:19 +0200

    fixed responsivenes

--- src/features/movie/production-companies/ProductionCompanies.jsx
+++ src/features/movie/production-companies/ProductionCompanies.jsx
@@ -6,21 +6,23 @@ const ProductionCompanies = ({ production_companies }) => {
   return (
     <>
       <h4 className="companies__title">Production companies:</h4>
-      {production_companies.map((company) => (
-        <li key={company.id} className="companies-item">
-          {company.logo_path ? (
-            <img
-              className="companies__logo"
-              src={`https://image.tmdb.org/t/p/w200${company.logo_path}`}
-              alt="company logo"
-            />
-          ) : (
-            <div className="companies__instead-logo">
-              <p>{company.name}</p>
-            </div>
-          )}
-        </li>
-      ))}
+      <ul className="companies__list">
+        {production_companies.map((company) => (
+          <li key={company.id} className="companies__item">
+            {company.logo_path ? (
+              <img
+                className="companies__logo"
+                src={`https://image.tmdb.org/t/p/w200${company.logo_path}`}
+                alt="company logo"
+              />
+            ) : (
+              <div className="companies__instead-logo">
+                <p>{company.name}</p>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
